71cd854570f528cec24d83fc16ced64197a1a800
sujal <103146049+sujalshukla9@users.noreply.github.com>
2025-12-11 10:17:25 -0800

    Update render-build.sh

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+
+# Install Python
+apt-get update
+apt-get install -y python3 python3-pip
+
 # Install yt-dlp
-pip install yt-dlp
+pip3 install yt-dlp
+
+echo "yt-dlp installed successfully!"
